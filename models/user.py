745271757a2c3f137a786d86ea4c95dabf5f0907
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-11 17:51:07 +0100

    add: DB storage for users

--- models/user.py
+++ models/user.py
@@ -3,11 +3,19 @@
 '''This module contains the class User. It encapsulates essential attributes
 and methods related to user management and interaction within the App'''
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import environ
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     '''User class inherits from BaseModel => user of
     The application'''
 
-    first_name = last_name = email = password = ''
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
