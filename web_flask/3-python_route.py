17956d69df17b3e01de9b453ccb9b737449ec31a
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-24 15:30:11 +0100

    route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """route the homepage"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """route the hbnb page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hbnb_c(text):
+    """route hbnb C"""
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route("/python")
+@app.route("/python/<text>", strict_slashes=False)
+def hbnb_python(text="is_cool"):
+    """route hbnb python"""
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+if __name__ == "__main__":
+    app.run()
