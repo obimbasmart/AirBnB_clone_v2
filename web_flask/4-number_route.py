983d97c43ce12e4f534177445c833f64ae983ef0
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-24 15:41:23 +0100

    show number from url

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """route the homepage"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """route the hbnb page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hbnb_c(text):
+    """route hbnb C"""
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def hbnb_python(text="is_cool"):
+    """route hbnb python"""
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+@app.route("/number/<int:number>", strict_slashes=False)
+def show_integer(number):
+    """show number"""
+    return f"{escape(number)} is a number"
+
+
+if __name__ == "__main__":
+    app.run()
