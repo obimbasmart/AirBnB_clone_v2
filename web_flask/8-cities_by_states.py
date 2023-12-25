19c3ff0490c78b539836ba1b8cef51193b878cba
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-25 03:16:30 +0100

    fix: incorrect url route

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,7 +8,7 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route("/cities_by_states", strict_slashes=False)
 def state_city_list():
     """show list of states and cities"""
     states = sorted([state for _, state in storage.all(
