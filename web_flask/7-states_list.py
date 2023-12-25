7d5b81205fa063e69c3495ecc1b49a9e348ca095
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-25 02:09:12 +0100

    add: load data from db

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""starts a Flask web application"""
+
+from flask import Flask, render_template
+from markupsafe import escape
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    """show list of states"""
+    states = sorted([state for _, state in storage.all(
+        State).items()], key=lambda x: x.name)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def close_db_connection(error=None):
+    """close database connections"""
+    storage.reload()
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
